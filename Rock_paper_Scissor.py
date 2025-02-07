a0083fb62874659aaeb79a14138d8ff3f90839fd
Karthikeyan S <160978576+Karthi-learner05@users.noreply.github.com>
2025-02-07 09:32:16 +0530

    Update Rock_paper_Scissor.py

--- Rock_paper_Scissor.py
+++ Rock_paper_Scissor.py
@@ -1,30 +1,46 @@
-from random import *
-def user():
-    while True:
-        choice1=input("Ente Your choice(rock,paper,scissor) = ").lower()
-        if choice1 in ['rock','paper','scissor']:
-            return choice1
-        else:
-            print("Invalid Choice!")
-def computer():
-    return choice(['rock','paper','scissor'])
-def win(c1,c2):
-    if c1==c2:
-        return "...Tie..."
-    elif((c1=='rock' and c2=='scissor' )or(c1=='paper' and c2=='rock' )or(c1=='scissor' and c2=='paper' )):
-        return "...You win..."
-    else:
-        return "...You Loss..."
-print("---....ROCK PAPER SCISSOR GAME....---")
-play='y'
-while play.lower()=='y':
-    print("\n")
-    u_choice=user()
-    c_choice=computer()
-    print("Your     choice = ",u_choice)
-    print("Computer choice = ",c_choice)
-    print(win(u_choice,c_choice))
-    print("\n")
-    play=input("Do you want to play again?(y/n) = ")
+import random
 
-print("\n---.....THANK YOU....---")
+class RPS_GAME:
+    player_score=0
+    computer_score=0
+    def com_choice(self):
+        return random.choice(["rock","paper","scissor"])
+    def user_choice(self):
+        choice=input("Enter your choice : ").lower()
+        if choice not in ["rock","paper","scissor","n","s"]:
+            print("Invalid Input...\n")
+            return self.user_choice()
+        return choice
+    def check_winner(self,user,com):
+        print("Computer choice : ",com)
+        if(user==com):
+            return "Tie\n"
+        elif(((user=="rock")and(com=="scissor"))or((user=="paper")and(com=="rock"))or((user=="scissor")and(com=="paper"))):
+            self.player_score+=1
+            return "You won the game 🏆 \n"
+        else:
+            self.computer_score+=1
+            return "Computer won the game 😔 \n"
+    def check_score(self):
+        print("----------------------------------------------------------------------")
+        print("|                                |                                   |")
+        print("|              PLAYER            |            COMPUTER               |")
+        print("----------------------------------------------------------------------")
+        print("|                                |                                   |")
+        print("|              ",self.player_score,"               |            ",self.computer_score,"                    |")
+        print("|                                |                                   |")
+        print("----------------------------------------------------------------------\n")
+if __name__=='__main__':
+    play="y"
+    rps=RPS_GAME()
+    print("-----------------WELCOME TO ROCK PAPER SCISSOR GAME-----------------\n Choices are,\n 1.rock\n 2.paper\n 3.scissor \n To EXIT input chice as n\n To CHECK SCORE input choice as s\n")
+    while(play=="y"):
+        user_option=rps.user_choice()
+        if(user_option=="s"):
+            rps.check_score()
+            continue
+        if(user_option=="n"):
+            print("\n----------THANK YOU 🙏 COME BACK AGAIN----------")
+            break
+        com_option=rps.com_choice()
+        print(rps.check_winner(user_option,com_option))
